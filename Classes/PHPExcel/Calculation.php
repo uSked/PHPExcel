fd71d706b7ba5125096ceb7c2db16bcaf44bf3d2
Marvin Herbold <521887+mherbold@users.noreply.github.com>
2024-06-06 09:59:32 -0700

    Update Calculation.php

--- Classes/PHPExcel/Calculation.php
+++ Classes/PHPExcel/Calculation.php
@@ -2669,11 +2669,11 @@ class PHPExcel_Calculation
         //    Basic validation that this is indeed a formula
         //    We return an empty array if not
         $formula = trim($formula);
-        if ((!isset($formula{0})) || ($formula{0} != '=')) {
+        if ((!isset($formula[0])) || ($formula[0] != '=')) {
             return array();
         }
         $formula = ltrim(substr($formula, 1));
-        if (!isset($formula{0})) {
+        if (!isset($formula[0])) {
             return array();
         }
 
