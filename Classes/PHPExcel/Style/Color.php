57b5d74d3117fff355dedbb49d39d2d1b4d03aaa
Marvin Herbold <521887+mherbold@users.noreply.github.com>
2024-06-06 12:29:19 -0700

    Update Color.php

--- Classes/PHPExcel/Style/Color.php
+++ Classes/PHPExcel/Style/Color.php
@@ -333,9 +333,9 @@ class PHPExcel_Style_Color extends PHPExcel_Style_Supervisor implements PHPExcel
         }
 
         $rgb = strtoupper(
-            str_pad(dechex($red), 2, '0', 0) .
-            str_pad(dechex($green), 2, '0', 0) .
-            str_pad(dechex($blue), 2, '0', 0)
+            str_pad(dechex((int)$red), 2, '0', 0) .
+            str_pad(dechex((int)$green), 2, '0', 0) .
+            str_pad(dechex((int)$blue), 2, '0', 0)
         );
         return (($rgba) ? 'FF' : '') . $rgb;
     }
